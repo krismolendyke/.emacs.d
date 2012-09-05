eed7901fd971b5382c44d21bb40d00d82d67e6f3
Kris <krismolendyke+github@gmail.com>
2012-09-04 20:27:29 -0400

    custom.el update.

--- custom.el
+++ custom.el
@@ -3,8 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("bf9d5728e674bde6a112979bd830cc90327850aaaf2e6f3cc4654f077146b406" default)))
  '(inhibit-startup-screen t)
- '(initial-scratch-message))
+ '(initial-scratch-message nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
