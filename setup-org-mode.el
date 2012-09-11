f7cd1636e8cde5d66c393e36a2a5024388d80dc9
Kris <krismolendyke+github@gmail.com>
2012-09-10 21:56:36 -0400

    Cleanup org-mode and add notes.org settings.

--- setup-org-mode.el
+++ setup-org-mode.el
@@ -1,11 +1,16 @@
 (require 'org-install)
 
 (setq org-directory (expand-file-name "org" google-drive-directory))
+
+;; Automatically insert a timestamp when a task is marked DONE.
 (setq org-log-done t)
 
+;; org-capture.
+(setq org-default-notes-file (expand-file-name "notes.org" org-directory))
+
+;; Agenda files.
 (setq org-agenda-files
-      (list
-       (expand-file-name "org/k20e.org" google-drive-directory)
-       (expand-file-name "org/monetate.org" google-drive-directory)))
+      (list (expand-file-name "k20e.org" org-directory)
+            (expand-file-name "work.org" org-directory)))
 
 (provide 'setup-org-mode)
