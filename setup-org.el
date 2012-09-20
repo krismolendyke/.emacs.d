5dc6ba3439eeaa7d9e826638dc5626a3e62a5ce0
Kris <krismolendyke+github@gmail.com>
2012-09-19 22:45:15 -0400

    org-mode - Set up publishing for IMT course.

--- setup-org.el
+++ setup-org.el
@@ -1,5 +1,6 @@
 (require 'org)
 (require 'org-install)
+(require 'org-publish)
 
 ;; Set the org directory.
 (setq org-directory (expand-file-name "org" google-drive-directory))
@@ -23,5 +24,30 @@
 (setq org-agenda-files
       (list (expand-file-name "k20e.org" org-directory)
             (expand-file-name "work.org" org-directory)))
+
+;; Publishing.
+(let* ((project-name "introduction-to-mathematical-thinking")
+       (org-component (format "%s-files" project-name))
+       (static-component (format "%s-static" project-name))
+       (project-directory project-name)
+       (root (expand-file-name project-directory org-directory))
+       (public (expand-file-name "public" root))
+       (static (expand-file-name "static" root)))
+  (setq org-publish-project-alist
+        `((,org-component
+           :base-directory ,root
+           :base-extension "org"
+           :publishing-directory ,public
+           :recursive t
+           :publishing-function org-publish-org-to-html
+           :headline-levels 6
+           :auto-preamble t)
+          (,static-component
+           :base-directory ,static
+           :base-extension "css\\|pdf"
+           :publishing-directory ,public
+           :recursive t
+           :publishing-function org-publish-attachment)
+          (,project-name :components (,org-component ,static-component)))))
 
 (provide 'setup-org)
