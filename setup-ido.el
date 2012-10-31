a1e8340ed85522bba4d169faedc7c3c567885838
Kris <krismolendyke+github@gmail.com>
2012-10-31 14:56:41 -0400

    ido-mode - Please don't go searching for my files as I type.

--- setup-ido.el
+++ setup-ido.el
@@ -1,7 +1,7 @@
 (require 'ido)
 (ido-mode t)
 
-;; Fuzzy matching!
-(setq ido-enable-flex-matching t)
+(setq ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length -1)
 
 (provide 'setup-ido)
