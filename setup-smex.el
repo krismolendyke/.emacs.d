d9cb2099278270d5b63f4a343be198953010680c
Kris <krismolendyke+github@gmail.com>
2012-11-27 08:41:58 -0500

    Share .smex-items across my machines.

--- setup-smex.el
+++ setup-smex.el
@@ -8,4 +8,7 @@
 ;; Keep execute-extended-command at hand just in case.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
+;; Share smex history across my machines.
+(setq smex-save-file (expand-file-name ".smex-items" dropbox-directory))
+
 (provide 'setup-smex)
