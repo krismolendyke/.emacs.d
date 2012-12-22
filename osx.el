f804995d470bb3444dd2bbdc6af90a13266045b6
Kris <krismolendyke+github@gmail.com>
2012-12-22 14:01:35 -0500

    Tame OS X trackpad scrolling... finally.

--- osx.el
+++ osx.el
@@ -4,4 +4,12 @@
 ;; Option as hyper.
 (setq ns-option-modifier 'hyper)
 
+ ;; Trackpad taming.
+(setq
+ mouse-wheel-scroll-amount '(0.0001)
+ mouse-wheel-progressive-speed nil
+ scroll-step 1
+ scroll-conservatively 10000
+ auto-window-vscroll nil)
+
 (provide 'osx)
