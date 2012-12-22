5901bce29b580880107534eb3c597c87a3e10e60
Kris <krismolendyke+github@gmail.com>
2012-12-22 14:01:57 -0500

    Tweak Python settings.

--- setup-python.el
+++ setup-python.el
@@ -1,4 +1,7 @@
 (add-hook 'python-mode-hook
-          #'(lambda () (toggle-truncate-lines 1)))
+          #'(lambda ()
+              (toggle-truncate-lines 1)
+              (setq fill-column 118)
+              (linum-mode 1)))
 
 (provide 'setup-python)
