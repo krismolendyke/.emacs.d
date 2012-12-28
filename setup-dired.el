4c3b765061abce3886888a95a3728176f7f4faf8
Kris <krismolendyke+github@gmail.com>
2012-12-28 12:12:54 -0500

    Enable auto-revert for dired.

--- setup-dired.el
+++ setup-dired.el
@@ -1,4 +1,8 @@
-;; Do not wrap lines.
-(add-hook 'dired-mode-hook #'(lambda () (setq truncate-lines 1)))
+(add-hook 'dired-mode-hook
+          #'(lambda ()
+              (auto-revert-mode 1)
+              (setq
+               auto-revert-verbose nil
+               truncate-lines 1)))
 
 (provide 'setup-dired)
