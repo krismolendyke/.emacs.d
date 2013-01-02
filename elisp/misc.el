8053ed858a689d1cdfc271bd0119eb3d15e76f9a
Kris <krismolendyke+github@gmail.com>
2013-01-02 14:01:33 -0500

    Add full moon functions.

--- elisp/misc.el
+++ elisp/misc.el
@@ -1,3 +1,6 @@
+(require 'calendar)
+(require 'lunar)
+
 ;; Inspired by http://whattheemacsd.com/key-bindings.el-01.html
 (defun goto-linum ()
   "Show line numbers before prompting for a line to go to."
@@ -25,3 +28,32 @@ value in the shell."
                  (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'")
                  "\n")))
          ":")))
+
+(defun full-moons-info ()
+  "Get a list of upcoming full moons information beginning with
+the current month.
+
+See `lunar-phase-list' and `lunar-phase-name'."
+  (let* ((current-date (calendar-current-date))
+         (current-month (car current-date))
+         (current-year (car (last current-date)))
+         (full-moon-phase-index 2)
+         (full-moons-info '()))
+    (dolist (phase (lunar-phase-list current-month current-year))
+      (if (= (car (last phase)) full-moon-phase-index)
+          (setq full-moons-info (cons phase full-moons-info))))
+    (reverse full-moons-info)))
+
+(defun full-moons ()
+  "Display a buffer containing upcoming full moons beginning with
+the current month."
+  (interactive)
+  (with-output-to-temp-buffer "*full-moons*"
+    (princ
+     (mapconcat
+      #'(lambda (x)
+          (format "%s %s" (calendar-date-string (car x)) (car (cdr x))))
+      (full-moons-info)
+      "\n"))))
+
+(provide 'misc)
