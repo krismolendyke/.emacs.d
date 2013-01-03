6aa75c80f7685612f50525da4b728c9e67e9d714
Kris <krismolendyke+github@gmail.com>
2013-01-02 20:47:37 -0500

    Run checkdoc. Insert IP from host-ip.

--- elisp/misc.el
+++ elisp/misc.el
@@ -1,9 +1,20 @@
+;;; misc.el --- miscellaneous functions
+
+;;; Commentary:
+;;
+;; Most of these functions are on a sort of "trial run" until I decide if I
+;; really need them around in a more permanent module.  There are a few key
+;; bindings to go along with a few functions as well.
+
+;;; Code:
 (require 'calendar)
+(require 'files)
 (require 'lunar)
+
 
 ;; Inspired by http://whattheemacsd.com/key-bindings.el-01.html
 (defun goto-linum ()
-  "Show line numbers before prompting for a line to go to."
+  "Show line numbers and prompt for a line number to go to."
   (interactive)
   (let ((linum-mode-previous-state
          (if (and (boundp 'linum-mode) linum-mode) 1 -1)))
@@ -18,21 +29,16 @@
 
 ;; Inspired by https://github.com/purcell/exec-path-from-shell
 (defun set-path-from-shell ()
-  "Set Emacs' PATH and exec-path variables to the current PATH
-value in the shell."
+  "Set `exec-path' to the current PATH value in the shell."
   (interactive)
   (setq exec-path
-        (split-string
-         (cadr (reverse
-                (split-string
-                 (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'")
-                 "\n")))
-         ":")))
+        (parse-colon-path (cadr (reverse (split-string
+                                          (shell-command-to-string
+                                           "$SHELL --login -i -c 'echo $PATH'")
+                                          "\n"))))))
 
 (defun full-moons-info ()
-  "Get a list of upcoming full moons information beginning with
-the current month.
-
+  "Get a list of upcoming full moons info beginning with the current month.
 See `lunar-phase-list' and `lunar-phase-name'."
   (let* ((current-date (calendar-current-date))
          (current-month (car current-date))
@@ -45,8 +51,7 @@ See `lunar-phase-list' and `lunar-phase-name'."
     (reverse full-moons-info)))
 
 (defun full-moons ()
-  "Display a buffer containing upcoming full moons beginning with
-the current month."
+  "Display upcoming full moons beginning with the current month."
   (interactive)
   (with-output-to-temp-buffer "*full-moons*"
     (princ
@@ -56,4 +61,15 @@ the current month."
       (full-moons-info)
       "\n"))))
 
+(require 'net-utils)
+
+(defun host-ip (host)
+  "Insert the current IP of the given HOST using `dns-lookup-program'.
+Similar to but simpler than `dns-lookup-host'."
+  (interactive "*sHost: ")
+  (insert (car (last (split-string (shell-command-to-string
+                                    (concat dns-lookup-program " " host)))))))
+
 (provide 'misc)
+
+;;; misc.el ends here
