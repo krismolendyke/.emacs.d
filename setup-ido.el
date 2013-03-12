f7e37bc715d83848f36a06ffe2edd07094fc2fdd
Kris <krismolendyke+github@gmail.com>
2013-03-12 13:22:32 -0400

    ido - Everywhere, shorten prompt, show dot for dired.

--- setup-ido.el
+++ setup-ido.el
@@ -1,8 +1,12 @@
 (require 'ido)
+
 (ido-mode t)
+(ido-everywhere t)
 
 (setq ido-enable-flex-matching t
       ido-auto-merge-work-directories-length -1
-      ido-create-new-buffer 'always)
+      ido-create-new-buffer 'always
+      ido-show-dot-for-dired t
+      ido-max-file-prompt-width 0.2)
 
 (provide 'setup-ido)
