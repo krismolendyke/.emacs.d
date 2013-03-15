30a9cd403f09ddcb35a66a156c1236b44a94d16c
Kris <krismolendyke+github@gmail.com>
2013-03-15 17:33:08 -0400

    Tweak ibuffer formats.

--- setup-ibuffer.el
+++ setup-ibuffer.el
@@ -2,4 +2,15 @@
           #'(lambda ()
               (setq ibuffer-truncate-lines t)))
 
+(setq ibuffer-formats '((mark modified read-only " "
+                              (name 24 24 :right :elide)
+                              " "
+                              (size 9 -1 :right)
+                              " "
+                              (mode 16 16 :left :elide)
+                              " " filename-and-process)
+                        (mark " "
+                              (name 16 -1)
+                              " " filename)))
+
 (provide 'setup-ibuffer)
