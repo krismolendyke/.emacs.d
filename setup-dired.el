237ce51faae05621e3cb14da757870f935d69a57
Kris <krismolendyke+github@gmail.com>
2013-03-16 13:26:33 -0400

    Adjust dired flagged face.

--- setup-dired.el
+++ setup-dired.el
@@ -3,6 +3,8 @@
               (auto-revert-mode 1)
               (setq
                auto-revert-verbose nil
-               truncate-lines 1)))
+               truncate-lines 1)
+              (set-face-foreground 'dired-flagged "tomato1")
+              (set-face-attribute 'dired-flagged nil :strike-through t)))
 
 (provide 'setup-dired)
