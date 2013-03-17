a087713ef486f48ba3ee2557cf2c5ee861033bdc
Kris <krismolendyke+github@gmail.com>
2013-03-17 08:06:48 -0400

    Bind ido-dired to C-x C-d as well.

--- setup-dired.el
+++ setup-dired.el
@@ -5,4 +5,8 @@
               (set-face-foreground 'dired-flagged "tomato1")
               (set-face-attribute 'dired-flagged nil :strike-through t)))
 
+;; C-x C-d is normally bound to `ido-list-directory' which I rarely need and
+;; often type when I intend to run `ido-dired'.
+(global-set-key (kbd "C-x C-d") 'ido-dired)
+
 (provide 'setup-dired)
