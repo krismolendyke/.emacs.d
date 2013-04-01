73880f97c23b4c3f5ad0a80da650096a5c8b3883
Kris <krismolendyke+github@gmail.com>
2013-04-01 11:48:04 -0400

    Use jinja2-mode for Soy Templates.

--- setup-jinja2.el
+++ setup-jinja2.el
@@ -1,3 +1,7 @@
 (require 'jinja2-mode)
 
+;; There is sadly no Emacs Soy Template mode... yet.
+(setq auto-mode-alist
+      (cons '("\\.soy" . jinja2-mode) auto-mode-alist))
+
 (provide 'setup-jinja2)
