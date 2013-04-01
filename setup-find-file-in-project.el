6b5eb9920e323e2f35c62d49ed2ab516aa2f4e7a
Kris <krismolendyke+github@gmail.com>
2013-04-01 11:44:10 -0400

    Tweak find file in project.
          - Bump file limit.  This may be excessive!
    
          - Add Soy Templates to the list of file types to find.
    
          - Add CSS to the list of file types to find.
    
          - Ignore the ./build/ directory in projects.
    
          - Show the full file paths when identically named files are found.

--- setup-find-file-in-project.el
+++ setup-find-file-in-project.el
@@ -1,6 +1,12 @@
 (require 'find-file-in-project)
 
-(setq ffip-limit 2048)
+(add-to-list 'ffip-patterns "*.css" t)
+(add-to-list 'ffip-patterns "*.soy" t)
+
+(setq ffip-limit 8192
+      ffip-find-options "-not -regex \".*/build.*\""
+      ffip-full-paths t)
+
 (global-set-key (kbd "C-x o") 'find-file-in-project)
 
 (provide 'setup-find-file-in-project)
