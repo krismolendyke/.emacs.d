8e8840d2018b2256bd62e45a4a7621882e4737c3
Kris <krismolendyke+github@gmail.com>
2013-04-03 16:27:49 -0400

    Revert "Use clojure-mode for ClojureScript."
    This reverts commit a97029fd985d30325b190879fe5576f7db345f1d.
    
        - Ends up that there is a clojurescript-mode!

--- setup-clojure.el
+++ setup-clojure.el
@@ -1,10 +1,6 @@
 (require 'clojure-mode)
 (require 'paredit)
 
-;; ClojureScript!
-(setq auto-mode-alist
-      (cons '("\\.cljs" . clojure-mode) auto-mode-alist))
-
 (add-hook 'clojure-mode-hook 'paredit-mode)
 
 (provide 'setup-clojure)
