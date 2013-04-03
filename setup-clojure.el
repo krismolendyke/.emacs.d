a97029fd985d30325b190879fe5576f7db345f1d
Kris <krismolendyke+github@gmail.com>
2013-04-03 16:20:04 -0400

    Use clojure-mode for ClojureScript.

--- setup-clojure.el
+++ setup-clojure.el
@@ -1,6 +1,10 @@
 (require 'clojure-mode)
 (require 'paredit)
 
+;; ClojureScript!
+(setq auto-mode-alist
+      (cons '("\\.cljs" . clojure-mode) auto-mode-alist))
+
 (add-hook 'clojure-mode-hook 'paredit-mode)
 
 (provide 'setup-clojure)
