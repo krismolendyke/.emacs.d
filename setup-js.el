364868e34e519f089f3866c3ca58642271f47486
Kris <krismolendyke+github@gmail.com>
2013-05-01 22:51:32 -0400

    Enable flycheck-mode when editing JavaScript.

--- setup-js.el
+++ setup-js.el
@@ -1,3 +1,7 @@
-(add-hook 'js-mode-hook (lambda () (linum-mode 1)))
+(require 'flycheck)
+
+(add-hook 'js-mode-hook (lambda ()
+                          (linum-mode 1)
+                          (flycheck-mode 1)))
 
 (provide 'setup-js)
