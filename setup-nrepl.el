4fed938578c8de58b4c6dda4bf147284b1d1b716
Kris <krismolendyke+github@gmail.com>
2013-07-08 11:39:51 -0400

    Add new settings to nrepl-setup.

--- setup-nrepl.el
+++ setup-nrepl.el
@@ -4,6 +4,10 @@
 
 (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
-(setq nrepl-popup-stacktraces nil)
+(setq nrepl-hide-special-buffers t
+      nrepl-popup-stacktraces-in-repl nil
+      nrepl-use-pretty-printing t
+      nrepl-history-file (expand-file-name "nrepl-history" dropbox-directory))
+(nrepl-enable-on-existing-clojure-buffers)
 
 (provide 'setup-nrepl)
