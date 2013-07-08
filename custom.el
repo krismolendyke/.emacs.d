2fa5e7853732d2b6d3348a76cdcc59568b9c6a18
Kris <krismolendyke+github@gmail.com>
2013-07-08 11:24:22 -0400

    Add new project to ibuffer filter groups.

--- custom.el
+++ custom.el
@@ -3,9 +3,54 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "38c9cb2514c5c185f8c84ee302e90a635979fb5b" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "cf2bb5e8046ca363183c87e8d33932f2a76a3d705b9db2721631777bbce92968" "4870e6cb6f0a70c14ee73db30b69a8a1f08d6ec9a689c366e88636fb81e8022d" "cc83fa4ffec1545d4bde6a44b1fb8431f9090874a22554920c709fa97338d0aa" "9117c98819cfdeb59780cb43e5d360ff8a5964d7dd9783b01708bda83098b9fd" "e439d894bf9406baf73056cf7e3c913ee5c794b6adadbbb9f614aebed0fd9ce7" "e992575f7c09459bfc190e6776b8f5f96964023e98267a87fb3094e7c9686776" "bf9d5728e674bde6a112979bd830cc90327850aaaf2e6f3cc4654f077146b406" default)))
- '(ibuffer-saved-filter-groups (quote (("default" ("predict" (filename . "\\(insight\\|predict\\)")) ("server" (filename . "-server")) ("elisp" (used-mode . emacs-lisp-mode)) ("org" (used-mode . org-mode)) ("*temp*" (name . "^\\*"))))))
- '(ibuffer-saved-filters (quote (("gnus" ((or (mode . message-mode) (mode . mail-mode) (mode . gnus-group-mode) (mode . gnus-summary-mode) (mode . gnus-article-mode)))) ("programming" ((or (mode . emacs-lisp-mode) (mode . cperl-mode) (mode . c-mode) (mode . java-mode) (mode . idl-mode) (mode . lisp-mode)))))))
+ '(custom-safe-themes
+   (quote
+    ("1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "38c9cb2514c5c185f8c84ee302e90a635979fb5b" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "cf2bb5e8046ca363183c87e8d33932f2a76a3d705b9db2721631777bbce92968" "4870e6cb6f0a70c14ee73db30b69a8a1f08d6ec9a689c366e88636fb81e8022d" "cc83fa4ffec1545d4bde6a44b1fb8431f9090874a22554920c709fa97338d0aa" "9117c98819cfdeb59780cb43e5d360ff8a5964d7dd9783b01708bda83098b9fd" "e439d894bf9406baf73056cf7e3c913ee5c794b6adadbbb9f614aebed0fd9ce7" "e992575f7c09459bfc190e6776b8f5f96964023e98267a87fb3094e7c9686776" "bf9d5728e674bde6a112979bd830cc90327850aaaf2e6f3cc4654f077146b406" default)))
+ '(ibuffer-saved-filter-groups
+   (quote
+    ((""
+      ("predict"
+       (filename . "\\(insight\\|predict\\)"))
+      ("server"
+       (filename . "-server"))
+      ("den"
+       (filename . "/den/"))
+      ("elisp"
+       (used-mode . emacs-lisp-mode))
+      ("org"
+       (used-mode . org-mode))
+      ("*temp*"
+       (name . "^\\*")))
+     ("default"
+      ("predict"
+       (filename . "\\(insight\\|predict\\)"))
+      ("server"
+       (filename . "-server"))
+      ("den"
+       (filename . "/den/"))
+      ("elisp"
+       (used-mode . emacs-lisp-mode))
+      ("org"
+       (used-mode . org-mode))
+      ("*temp*"
+       (name . "^\\*"))))))
+ '(ibuffer-saved-filters
+   (quote
+    (("gnus"
+      ((or
+        (mode . message-mode)
+        (mode . mail-mode)
+        (mode . gnus-group-mode)
+        (mode . gnus-summary-mode)
+        (mode . gnus-article-mode))))
+     ("programming"
+      ((or
+        (mode . emacs-lisp-mode)
+        (mode . cperl-mode)
+        (mode . c-mode)
+        (mode . java-mode)
+        (mode . idl-mode)
+        (mode . lisp-mode)))))))
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil))
 
