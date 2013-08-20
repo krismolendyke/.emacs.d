ad368a16e537e7ec6f3ccb6a32758095494b1cf5
Kris <krismolendyke+github@gmail.com>
2013-08-20 08:10:21 -0400

    Autopoir doc quotes in Emacs Lisp.

--- setup-emacs-lisp.el
+++ setup-emacs-lisp.el
@@ -1,3 +1,12 @@
-(add-hook 'emacs-lisp-mode-hook (lambda () (electric-indent-mode)))
+(require 'autopair)
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (electric-indent-mode)
+            ;; autopair `' when writing comments or strings.
+            (push '(?` . ?')
+                  (getf autopair-extra-pairs :comment))
+            (push '(?` . ?')
+                  (getf autopair-extra-pairs :string))))
 
 (provide 'setup-emacs-lisp)
