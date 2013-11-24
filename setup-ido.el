fc0fd7a5550da16513ef200ddbb0d9272f22b7c1
Kris <krismolendyke+github@gmail.com>
2013-11-24 12:17:09 -0500

    Try out flx-ido.

--- setup-ido.el
+++ setup-ido.el
@@ -1,12 +1,15 @@
+(require 'flx-ido)
 (require 'ido)
 
 (ido-mode t)
 (ido-everywhere t)
+(flx-ido-mode t)
 
 (setq ido-enable-flex-matching t
       ido-auto-merge-work-directories-length -1
       ido-create-new-buffer 'always
       ido-show-dot-for-dired t
-      ido-max-file-prompt-width 0.2)
+      ido-max-file-prompt-width 0.2
+      ido-use-faces nil)
 
 (provide 'setup-ido)
