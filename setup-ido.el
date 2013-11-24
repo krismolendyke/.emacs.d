fc87a2aaa77b0586a1dfaeb5b5fa58c78646186e
Kris <krismolendyke+github@gmail.com>
2013-11-24 18:12:11 -0500

    Try out ido-vertical-mode.

--- setup-ido.el
+++ setup-ido.el
@@ -1,7 +1,25 @@
 (require 'flx-ido)
 (require 'ido)
+(require 'ido-vertical-mode)
+
+;; Boring arrows be gone!
+(setq ido-vertical-decorations '("\n ► "    ; left bracket around prospect list
+                                 ""         ; right bracket around prospect list
+                                 "\n   "    ; separator between prospects, depends on `ido-separator`
+                                 "\n   ..." ; inserted at the end of a truncated list of prospects
+                                 "["        ; left bracket around common match string
+                                 "]"        ; right bracket around common match string
+                                 " [No match]"
+                                 " [Matched]"
+                                 " [Not readable]"
+                                 " [Too big]"
+                                 " [Confirm]"
+                                 "\n ► "    ; left bracket around the sole remaining completion
+                                 ""         ; right bracket around the sole remaining completion
+                                 ))
 
 (ido-mode t)
+(ido-vertical-mode t)
 (ido-everywhere t)
 (flx-ido-mode t)
 
