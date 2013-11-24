175e89aaa38345cf2a480c2782428397367302cb
Kris <krismolendyke+github@gmail.com>
2013-11-24 16:22:35 -0500

    Highlight parentheses in Clojure.

--- setup-highlight-parentheses.el
+++ setup-highlight-parentheses.el
@@ -2,7 +2,8 @@
 
 (dolist (hook '(emacs-lisp-mode-hook
                 lisp-mode-hook
-                cider-repl-mode-hook))
+                cider-repl-mode-hook
+                clojure-mode-hook))
   (add-hook hook #'(lambda ()
                      (highlight-parentheses-mode))))
 
