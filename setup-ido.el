2fb7d690d31faa31b76b52db0b9db0ca694e4818
Kris <krismolendyke+github@gmail.com>
2013-11-29 09:52:34 -0500

    align-regex.

--- setup-ido.el
+++ setup-ido.el
@@ -3,19 +3,19 @@
 (require 'ido-vertical-mode)
 
 ;; Boring arrows be gone!
-(setq ido-vertical-decorations '("\n    "    ; left bracket around prospect list
-                                 ""         ; right bracket around prospect list
-                                 "\n    "    ; separator between prospects, depends on `ido-separator`
-                                 "\n    ▼"   ; inserted at the end of a truncated list of prospects
-                                 "["        ; left bracket around common match string
-                                 "]"        ; right bracket around common match string
-                                 " ✘"       ; no match
-                                 " ✔"       ; matched
+(setq ido-vertical-decorations '("\n    "  ; left bracket around prospect list
+                                 ""        ; right bracket around prospect list
+                                 "\n    "  ; separator between prospects, depends on `ido-separator`
+                                 "\n    ▼" ; inserted at the end of a truncated list of prospects
+                                 "["       ; left bracket around common match string
+                                 "]"       ; right bracket around common match string
+                                 " ✘"      ; no match
+                                 " ✔"      ; matched
                                  " [Not readable]"
                                  " [Too big]"
-                                 " ?"       ; confirm
-                                 "\n    "    ; left bracket around the sole remaining completion
-                                 " ✔"       ; right bracket around the sole remaining completion
+                                 " ?"      ; confirm
+                                 "\n    "  ; left bracket around the sole remaining completion
+                                 " ✔"      ; right bracket around the sole remaining completion
                                  ))
 
 (custom-set-faces '(ido-first-match ((t (:foreground "green4"))))
