3742e6c9d662d14d66c2747eaf98702c9bede03b
Kris <krismolendyke+github@gmail.com>
2014-07-22 16:50:30 -0400

    Namespace init variables and functions.

--- init.el
+++ init.el
@@ -7,33 +7,33 @@
 
 ;;; Code:
 
-(defvar site-lisp-directory
+(defvar k20e/site-lisp-directory
   (expand-file-name "site-lisp" user-emacs-directory)
   "Local libraries.")
 
-(defvar elisp-directory
+(defvar k20e/elisp-directory
   (expand-file-name "elisp" user-emacs-directory)
   "Stuff that I have developed.")
 
-(defvar org-lisp-directory
-  (expand-file-name "lisp" (expand-file-name "org-mode" site-lisp-directory))
+(defvar k20e/org-lisp-directory
+  (expand-file-name "lisp" (expand-file-name "org-mode" k20e/site-lisp-directory))
   "The directory contaiting `org-mode' Emacs Lisp files.")
 
-(defvar dropbox-directory
+(defvar k20e/dropbox-directory
   (expand-file-name "~/Desktop/Dropbox")
   "Dropbox home.")
 
-(defvar google-drive-directory
+(defvar k20e/google-drive-directory
   (expand-file-name "~/Google Drive")
   "Google Drive home.")
 
-(defun setup-cask-and-pallet ()
+(defun k20e/setup-cask-and-pallet ()
   "Package managment goodness."
   (require 'cask "~/.cask/cask.el")
   (cask-initialize)
   (require 'pallet))
 
-(defun no-bars-held ()
+(defun k20e/no-bars-held ()
   "Turn off tool, scroll, and menu bars when appropriate.
 Only turn off the menu bar running in a terminal window."
   (setq inhibit-startup-echo-area-message "kris")
@@ -44,44 +44,44 @@ Only turn off the menu bar running in a terminal window."
            (fboundp 'menu-bar-mode))
       (menu-bar-mode -1)))
 
-(defun setup-load-path ()
+(defun k20e/setup-load-path ()
   "Add custom directories to `load-path'."
-  (dolist (directory (list site-lisp-directory
-                           elisp-directory
-                           org-lisp-directory))
+  (dolist (directory (list k20e/site-lisp-directory
+                           k20e/elisp-directory
+                           k20e/org-lisp-directory))
     (add-to-list 'load-path directory))
 
   ;; Add external projects to load path.
-  (dolist (project (directory-files site-lisp-directory t "\\w+"))
+  (dolist (project (directory-files k20e/site-lisp-directory t "\\w+"))
     (when (file-directory-p project)
       (add-to-list 'load-path project))))
 
-(defun load-custom-elisp ()
-  "Load custom Emacs Lisp files in `elisp-directory'."
-  (dolist (file (directory-files elisp-directory t "\\w+"))
+(defun k20e/load-custom-elisp ()
+  "Load custom Emacs Lisp files in `k20e/elisp-directory'."
+  (dolist (file (directory-files k20e/elisp-directory t "\\w+"))
     (when (file-regular-p file)
       (load file))))
 
-(defun restore-desktop ()
+(defun k20e/restore-desktop ()
   "Restore the state of buffers from the last session."
   (require 'desktop)
-  (setq desktop-path (list dropbox-directory)
+  (setq desktop-path (list k20e/dropbox-directory)
         desktop-load-locked-desktop t)
   (desktop-save-mode 1)
-  (desktop-read dropbox-directory))
+  (desktop-read k20e/dropbox-directory))
 
-(defun after-init ()
+(defun k20e/after-init ()
   "Perform complex post-initialization."
   (require 'org)
   (dolist (elt (directory-files user-emacs-directory t "\\.org$" t))
     (org-babel-load-file elt t)))
 
 (setq source-directory "/Library/Caches/Homebrew/emacs--git")
-(setup-cask-and-pallet)
-(no-bars-held)
-(setup-load-path)
-(load-custom-elisp)
-(restore-desktop)
-(add-hook 'after-init-hook 'after-init)
+(k20e/setup-cask-and-pallet)
+(k20e/no-bars-held)
+(k20e/setup-load-path)
+(k20e/load-custom-elisp)
+(k20e/restore-desktop)
+(add-hook 'after-init-hook 'k20e/after-init)
 
 ;;; init.el ends here
