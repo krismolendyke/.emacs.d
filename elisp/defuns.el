66f865d51591197e9cf6e0d78dd41c297f82f43a
Kris <krismolendyke+github@gmail.com>
2014-12-28 17:04:13 -0500

    Migrate defuns.

--- elisp/defuns.el
+++ elisp/defuns.el
@@ -7,9 +7,7 @@
 ;; bindings to go along with a few functions as well.
 
 ;;; Code:
-(require 'calendar)
 (require 'files)
-(require 'lunar)
 
 (defun file-contents-as-string (filename)
   "Return the given FILENAME contents as a string."
@@ -33,29 +31,5 @@ Optionally exclude EXCLUDES from the result list."
 (defun blank-line-p ()
   "Is point currently on a blank line?"
   (looking-at "[ \t]*$"))
-
-(defun full-moons-info ()
-  "Get a list of upcoming full moons info beginning with the current month.
-See `lunar-phase-list' and `lunar-phase-name'."
-  (let* ((current-date (calendar-current-date))
-         (current-month (car current-date))
-         (current-year (car (last current-date)))
-         (full-moon-phase-index 2)
-         (full-moons-info '()))
-    (dolist (phase (lunar-phase-list current-month current-year))
-      (if (= (car (last phase)) full-moon-phase-index)
-          (setq full-moons-info (cons phase full-moons-info))))
-    (reverse full-moons-info)))
-
-(defun full-moons ()
-  "Display upcoming full moons beginning with the current month."
-  (interactive)
-  (with-output-to-temp-buffer "*full-moons*"
-    (princ
-     (mapconcat
-      #'(lambda (x)
-          (format "%s %s" (calendar-date-string (car x)) (car (cdr x))))
-      (full-moons-info)
-      "\n"))))
 
 ;;; defuns.el ends here
