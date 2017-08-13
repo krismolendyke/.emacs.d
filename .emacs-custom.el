7a9f00a98618d28059df127e76007b346daa2db2
Kris <krismolendyke@users.noreply.github.com>
2017-08-13 13:47:27 -0400

    Remove solarized themes

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" default)))
+    ("1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" default)))
  '(package-selected-packages
    (quote
     (flx yasnippet yaml-mode writeroom-mode web-mode typescript-mode toml-mode toml smex rust-mode pyvenv paredit paradox pallet multiple-cursors multi-term markdown-mode lua-mode keyfreq htmlize highlight-parentheses groovy-mode go-mode gitignore-mode gitconfig-mode git-timemachine geiser flycheck-yamllint find-file-in-project expand-region exec-path-from-shell dockerfile-mode dash-at-point counsel cider buffer-move arduino-mode ansible-doc ag)))
