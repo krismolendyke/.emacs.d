bf1cef619ccc60540333b839c5affa72ed57fa97
Kris <krismolendyke@users.noreply.github.com>
2017-12-27 07:35:32 -0500

    Sync custom w/ Cask changes

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -8,7 +8,7 @@
     ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(package-selected-packages
    (quote
-    (tide color-theme-sanityinc-tomorrow flx yasnippet yaml-mode writeroom-mode web-mode typescript-mode toml-mode toml smex rust-mode pyvenv paredit paradox pallet multiple-cursors multi-term markdown-mode lua-mode keyfreq htmlize highlight-parentheses groovy-mode go-mode gitignore-mode gitconfig-mode git-timemachine geiser flycheck-yamllint find-file-in-project expand-region exec-path-from-shell dockerfile-mode dash-at-point counsel cider buffer-move arduino-mode ansible-doc ag)))
+    (cargo flycheck-rust tide color-theme-sanityinc-tomorrow flx yasnippet yaml-mode writeroom-mode web-mode typescript-mode toml-mode toml smex rust-mode pyvenv paredit paradox pallet multiple-cursors multi-term markdown-mode lua-mode keyfreq htmlize highlight-parentheses groovy-mode go-mode gitignore-mode gitconfig-mode git-timemachine geiser flycheck-yamllint find-file-in-project expand-region exec-path-from-shell dockerfile-mode dash-at-point counsel cider buffer-move arduino-mode ansible-doc ag)))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
