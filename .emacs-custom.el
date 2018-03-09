fad8e54d6ac15fd5ba1e798a48af14cf0207a37e
Kris <krismolendyke@users.noreply.github.com>
2018-03-09 17:06:11 -0500

    Prune packages

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,13 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(sanityinc-tomorrow-blue))
  '(custom-safe-themes
-   (quote
-    ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+   '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(package-selected-packages
-   (quote
-    (cargo flycheck-rust tide color-theme-sanityinc-tomorrow flx yasnippet yaml-mode writeroom-mode web-mode typescript-mode toml-mode toml smex rust-mode pyvenv paredit paradox pallet multiple-cursors multi-term markdown-mode lua-mode keyfreq htmlize highlight-parentheses groovy-mode go-mode gitignore-mode gitconfig-mode git-timemachine geiser flycheck-yamllint find-file-in-project expand-region exec-path-from-shell dockerfile-mode dash-at-point counsel cider buffer-move arduino-mode ansible-doc ag)))
- '(paradox-github-token t))
+   '(avy cargo flycheck-rust tide color-theme-sanityinc-tomorrow flx yasnippet yaml-mode web-mode typescript-mode toml-mode toml smex rust-mode paredit paradox pallet multiple-cursors multi-term markdown-mode keyfreq htmlize highlight-parentheses groovy-mode go-mode gitignore-mode gitconfig-mode git-timemachine geiser flycheck-yamllint find-file-in-project expand-region exec-path-from-shell counsel buffer-move arduino-mode ansible-doc ag))
+ '(paradox-github-token t)
+ '(tramp-syntax 'default nil (tramp))
+ '(yas-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
