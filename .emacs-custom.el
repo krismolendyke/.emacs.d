8eee1aef92a1d95e083e3d84ab2709c046c7e3c3
Kris <krismolendyke@users.noreply.github.com>
2018-04-06 07:48:33 -0400

    Update custom

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -5,10 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(sanityinc-tomorrow-blue))
  '(custom-safe-themes
-   '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
+   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(package-selected-packages
-   '(avy cargo flycheck-rust tide color-theme-sanityinc-tomorrow flx yasnippet yaml-mode web-mode typescript-mode toml-mode toml smex rust-mode paredit paradox pallet multiple-cursors multi-term markdown-mode keyfreq htmlize highlight-parentheses groovy-mode go-mode gitignore-mode gitconfig-mode git-timemachine geiser flycheck-yamllint find-file-in-project expand-region exec-path-from-shell counsel buffer-move arduino-mode ansible-doc ag))
+   '(git-commit smart-mode-line avy cargo flycheck-rust tide color-theme-sanityinc-tomorrow flx yasnippet yaml-mode web-mode typescript-mode toml-mode toml smex rust-mode paredit paradox pallet multiple-cursors multi-term markdown-mode keyfreq htmlize highlight-parentheses groovy-mode go-mode gitignore-mode gitconfig-mode git-timemachine geiser flycheck-yamllint find-file-in-project expand-region exec-path-from-shell counsel buffer-move arduino-mode ansible-doc ag))
  '(paradox-github-token t)
+ '(size-indication-mode t)
  '(tramp-syntax 'default nil (tramp))
  '(yas-global-mode t))
 (custom-set-faces
