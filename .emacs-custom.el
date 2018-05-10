33e5b74cdeb0e491ecc6b9505409b9e34b537b15
Kris <krismolendyke@users.noreply.github.com>
2018-05-10 11:00:49 -0400

    Update custom

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -7,4 +7,11 @@
  '(custom-safe-themes
    '("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "82d2cac368ccdec2\nfcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default))
  '(package-selected-packages
-   '(git-commit smart-mode-line avy cargo flycheck-rust tide color-theme-sanityinc-tomorrow flx yasnippet yaml-mode web-mode typescript-mode toml-mode toml smex rust-mode paredit paradox pallet multiple-cursors multi-term markdown-mode keyfreq htmlize highlight-parentheses groovy-mode go-mode gitignore-mode gitconfig-mode git-timemachine geiser flycheck-yamllint find-file-in-project expand-region exec-path-from-shell counsel buffer-move arduino-mode ansible-doc ag)))
+   '(git-commit smart-mode-line avy cargo flycheck-rust tide color-theme-sanityinc-tomorrow flx yasnippet yaml-mode web-mode typescript-mode toml-mode toml smex rust-mode paredit paradox pallet multiple-cursors multi-term markdown-mode keyfreq htmlize highlight-parentheses groovy-mode go-mode gitignore-mode gitconfig-mode git-timemachine geiser flycheck-yamllint find-file-in-project expand-region exec-path-from-shell counsel buffer-move arduino-mode ansible-doc ag))
+ '(paradox-github-token t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
