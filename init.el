d437c9f64dcb79f4b0defb42e1653159cb772d81
Kris <krismolendyke@users.noreply.github.com>
2020-09-10 14:11:20 -0400

    Update packages, gh token

--- init.el
+++ init.el
@@ -142,7 +142,10 @@ Only turn off the menu bar running in a terminal window."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+   '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
+ '(package-selected-packages
+   '(use-package yasnippet yaml-mode web-mode visual-fill-column toml-mode toml tide terraform-mode smex queue protobuf-mode paredit paradox pallet ox-tufte ox-jira ns-auto-titlebar multiple-cursors multi-term lsp-mode kotlin-mode keyfreq htmlize highlight-parentheses highlight-indent-guides groovy-mode go-rename go-guru go-errcheck go-eldoc gitignore-mode gitconfig-mode git-timemachine git-commit geiser flycheck-yamllint flycheck-rust flx find-file-in-project feature-mode expand-region exec-path-from-shell dockerfile-mode counsel company-go color-theme-sanityinc-tomorrow cargo buffer-move blacken avy arduino-mode ag 0blayout))
+ '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
