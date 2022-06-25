a6f5aefd5948d43c47e18c8e08e7cf5e6eb2dce2
Kris <krismolendyke@users.noreply.github.com>
2022-06-25 18:21:58 -0400

    Update custome

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -5,9 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(sanityinc-tomorrow-bright))
  '(custom-safe-themes
-   '("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
+   '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
  '(package-selected-packages
-   '(use-package yasnippet yaml-mode web-mode visual-fill-column toml-mode toml tide terraform-mode smex queue protobuf-mode paredit paradox pallet ox-tufte ox-jira ns-auto-titlebar multiple-cursors multi-term lsp-mode kotlin-mode keyfreq htmlize highlight-parentheses highlight-indent-guides groovy-mode go-rename go-guru go-errcheck go-eldoc gitignore-mode gitconfig-mode git-timemachine git-commit geiser flycheck-yamllint flycheck-rust flx find-file-in-project feature-mode expand-region exec-path-from-shell dockerfile-mode counsel company-go color-theme-sanityinc-tomorrow cargo buffer-move blacken avy arduino-mode ag 0blayout))
+   '(zig-mode use-package yasnippet yaml-mode web-mode visual-fill-column toml-mode toml tide terraform-mode smex queue protobuf-mode paredit paradox pallet ox-tufte ox-jira ns-auto-titlebar multiple-cursors multi-term lsp-mode kotlin-mode keyfreq htmlize highlight-parentheses highlight-indent-guides groovy-mode go-rename go-guru go-errcheck go-eldoc gitignore-mode gitconfig-mode git-timemachine git-commit geiser flycheck-yamllint flycheck-rust flx find-file-in-project feature-mode expand-region exec-path-from-shell dockerfile-mode counsel company-go color-theme-sanityinc-tomorrow cargo buffer-move blacken avy arduino-mode ag 0blayout))
  '(paradox-github-token t)
  '(safe-local-variable-values
    '((flycheck-disabled-checkers emacs-lisp-checkdoc)
@@ -19,4 +19,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(button ((t (:box nil))))
+ '(fixed-pitch ((t (:family "PragmataPro Mono Liga" :height 0.9))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-block-begin-line ((t (:inherit fixed-pitch))))
+ '(org-block-end-line ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-date ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-done ((t (:inherit (shadow fixed-pitch)))))
+ '(org-drawer ((t (:inherit (shadow fixed-pitch)))))
+ '(org-ellipsis ((t (:inherit (shadow fixed-pitch)))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-quote ((t (:inherit (shadow)))))
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch)))))
+ '(org-todo ((t (:inherit (shadow fixed-pitch)))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+ '(variable-pitch ((t (:family "ETBembo" :height 1.3)))))
