1362e8e988ab2a147cfa5f62b954e4d6ab01b9a6
Kris Molendyke <krismolendyke@users.noreply.github.com>
2023-09-05 19:43:04 -0400

    WIP first working hack at replacing cask w/ use-package for 29.1

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -7,12 +7,12 @@
  '(custom-safe-themes
    '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
  '(package-selected-packages
-   '(use-package yasnippet yaml-mode web-mode visual-fill-column toml-mode toml tide terraform-mode smex queue protobuf-mode paredit paradox pallet ox-tufte ox-jira ns-auto-titlebar multiple-cursors multi-term lsp-mode kotlin-mode keyfreq htmlize highlight-parentheses highlight-indent-guides groovy-mode go-rename go-guru go-errcheck go-eldoc gitignore-mode gitconfig-mode git-timemachine git-commit geiser flycheck-yamllint flycheck-rust flx find-file-in-project feature-mode expand-region exec-path-from-shell dockerfile-mode counsel company-go color-theme-sanityinc-tomorrow cargo buffer-move blacken avy arduino-mode ag 0blayout))
+   '(rust-mode lunar ligature bazel use-package yasnippet yaml-mode web-mode visual-fill-column toml-mode toml tide terraform-mode smex queue protobuf-mode paredit paradox pallet ox-tufte ox-jira ns-auto-titlebar multiple-cursors multi-term lsp-mode kotlin-mode keyfreq htmlize highlight-parentheses highlight-indent-guides groovy-mode go-rename go-guru go-errcheck go-eldoc gitignore-mode gitconfig-mode git-timemachine git-commit geiser flycheck-yamllint flycheck-rust flx find-file-in-project feature-mode expand-region exec-path-from-shell dockerfile-mode counsel company-go color-theme-sanityinc-tomorrow cargo buffer-move blacken avy arduino-mode ag 0blayout))
  '(paradox-github-token t)
  '(safe-local-variable-values
    '((flycheck-disabled-checkers emacs-lisp-checkdoc)
-     (major-mode . gitignore-mode)
-     (major-mode . gitignore)))
+	 (major-mode . gitignore-mode)
+	 (major-mode . gitignore)))
  '(yas-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -30,7 +30,7 @@
  '(org-done ((t (:inherit (shadow fixed-pitch)))))
  '(org-drawer ((t (:inherit (shadow fixed-pitch)))))
  '(org-ellipsis ((t (:inherit (shadow fixed-pitch)))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))) t)
  '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
  '(org-property-value ((t (:inherit fixed-pitch))) t)
  '(org-quote ((t (:inherit (shadow)))))
--- init.el
+++ init.el
@@ -35,10 +35,6 @@ decoupled from the Emacs distribution package.")
   (expand-file-name "~/Google")
   "Google Drive home.")
 
-(defvar k20e/use-package-directory
-  (expand-file-name "use-package" k20e/site-lisp-directory)
-  "The directory containing `use-package' files.")
-
 (when (string-equal system-type "darwin")
                     ; TODO move into macos section in custom
   (setq mac-command-modifier 'meta
@@ -48,20 +44,6 @@ decoupled from the Emacs distribution package.")
     (string-trim (shell-command-to-string
                   (string-join `(,(executable-find "brew") "--cache") " ")))
     "Homebrew cache."))
-
-(defun k20e/setup-use-package ()
-  "https://github.com/jwiegley/use-package"
-  (eval-when-compile
-    (add-to-list 'load-path k20e/use-package-directory)
-    (require 'use-package))
-
-  (with-eval-after-load 'info
-    (info-initialize)
-    (add-to-list 'Info-directory-list k20e/use-package-directory))
-
-  (require 'package)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t))
 
 (defun k20e/no-bars-held ()
   "Turn off tool, scroll, and menu bars when appropriate.
@@ -129,6 +111,12 @@ Only turn off the menu bar running in a terminal window."
   (setq source-directory
         (string-join `(,k20e/brew-cache-directory "emacs--git") "/")))
 
+(defun k20e/setup-use-package ()
+  "Configure use-package."
+  (use-package package
+    :config
+    (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)))
+
 (k20e/setup-use-package)
 (k20e/no-bars-held)
 (k20e/setup-load-path)
@@ -146,8 +134,6 @@ Only turn off the menu bar running in a terminal window."
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
- '(package-selected-packages
-   '(use-package yasnippet yaml-mode web-mode visual-fill-column toml-mode toml tide terraform-mode smex queue protobuf-mode paredit paradox pallet ox-tufte ox-jira ns-auto-titlebar multiple-cursors multi-term lsp-mode kotlin-mode keyfreq htmlize highlight-parentheses highlight-indent-guides groovy-mode go-rename go-guru go-errcheck go-eldoc gitignore-mode gitconfig-mode git-timemachine git-commit geiser flycheck-yamllint flycheck-rust flx find-file-in-project feature-mode expand-region exec-path-from-shell dockerfile-mode counsel company-go color-theme-sanityinc-tomorrow cargo buffer-move blacken avy arduino-mode ag 0blayout))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
